f300ae1d5fd8ad717649f0552e43574cfe591acc
SakthivelSwe <sakthivelnaruto@gmail.com>
2024-01-15 11:03:44 +0530

    feat: created the Skills and SkillCards

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css'
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero.jsx/Hero";
+import Skills from "./components/Skills/Skills";
 const App = ()=>{
     return(
         <>
         <Navbar/>
         <div className="container">
             <Hero/>
+            <Skills/>
         </div>
         </>
     )
---
+++ src/Utils/data.js
@@ -0,0 +1,45 @@
+export const SKILLS = [
+    {
+        title:"Frontend",
+        icon:"./assets/images/frontendicon.png",
+        skills:[
+            {skill:"HTML5",percentage:"80%"},
+            {skill:"CSS (TailwindCSS)",percentage:"90%"},
+            {skill:"JavaScript",percentage:"70%"},
+            {skill:"React.js",percentage:"75%"},  
+        ],
+
+    },
+    {
+        title:"Backend",
+        icon:"./assets/images/backendicon.png",
+        skills:[
+            {skill:"Java",percentage:"85%"},
+            {skill:"J2EE",percentage:"60%"},
+            {skill:"Hibernate/JPA Framework",percentage:"70%"},
+            {skill:"Spring Framework",percentage:"50%"},  
+        ],
+    },
+    {
+        title:"Tools",
+        icon:"./assets/images/toolsicon.png",
+        skills:[
+            {skill:"Git & GitHub",percentage:"90%"},
+            {skill:"Visual Studio Code",percentage:"75%"},
+            {skill:"Eclipse IDE",percentage:"80%"},
+            {skill:"Maven",percentage:"50%"},  
+        ],
+    },
+    {
+        title:"Soft Skills",
+        icon:"./assets/images/softskilicon.png",
+        skills:[
+            {skill:"Problem-solving",percentage:"80%"},
+            {skill:"Collaboration",percentage:"85%"},
+            {skill:"Attention to Detail",percentage:"90%"},
+            {skill:"Adaptability",percentage:"90%"},  
+        ]
+    },
+
+
+]
